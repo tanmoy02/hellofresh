155bed0af15daaa99099c61b9c8372a50ba2f1e5
Tanmoy Adhikary <adhikary.tanmoy@gmail.com>
2021-03-21 13:59:24 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,45 +8,44 @@ window.addEventListener('scroll', () => {
     document.documentElement.style.setProperty('--scroll-bottom', pos + 'px')
 })
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    var words = document.getElementsByClassName('word');
-    var wordArray = [];
-    var currentWord = 0;
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
 
-    words[currentWord].style.opacity = 1;
-    for (var i = 0; i < words.length; i++) {
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
     splitLetters(words[i]);
-    }
+}
 
-    function changeWord() {
+function changeWord() {
     var cw = wordArray[currentWord];
     var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
     for (var i = 0; i < cw.length; i++) {
         animateLetterOut(cw, i);
     }
-    
+
     for (var i = 0; i < nw.length; i++) {
         nw[i].className = 'letter behind';
         nw[0].parentElement.style.opacity = 1;
         animateLetterIn(nw, i);
     }
-    
-    currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-    }
 
-    function animateLetterOut(cw, i) {
+    currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
     setTimeout(function() {
             cw[i].className = 'letter out';
     }, i*80);
-    }
+}
 
-    function animateLetterIn(nw, i) {
+function animateLetterIn(nw, i) {
     setTimeout(function() {
             nw[i].className = 'letter in';
     }, 340+(i*80));
-    }
+}
 
-    function splitLetters(word) {
+function splitLetters(word) {
     var content = word.innerHTML;
     word.innerHTML = '';
     var letters = [];
@@ -57,10 +56,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
         word.appendChild(letter);
         letters.push(letter);
     }
-    
-    wordArray.push(letters);
-    }
 
-    changeWord();
-    setInterval(changeWord, 4000);
-})
+    wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
